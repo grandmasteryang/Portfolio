eb932d3a05ee9f54bf82c1b6be69479edf8a19c4
grandmasteryang <xuyang2016@u.northwestern.edu>
2018-01-09 15:36:33 -0800

    add headshot and transitions

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-$(".downArrow").click(function() {
-	$('html,body').animate({
-	    scrollTop: $(".projectsPage").offset().top},
-	    'slow');
-});
-
 $(".aboutText").hide();
 $(".reachOutText").hide();
 $('#home').css({"color": "#009EDB"});
 
 document.getElementById('home').addEventListener('click', function(event) {
-    $(".introText").show();
-    $(".aboutText").hide();
-    $(".reachOutText").hide();
+    $(".introText").fadeIn("slow");
+    $(".aboutText").fadeOut("slow");
+    $(".reachOutText").fadeOut("slow");
     $('#home').css({"color": "#009EDB"});
     $('#about').css({"color": "#777777"});
     $('#reachOut').css({"color": "#777777"});
 }, false);
 
 document.getElementById('about').addEventListener('click', function(event) {
-    $(".introText").hide();
-    $(".reachOutText").hide();
-    $(".aboutText").show();
+    $(".introText").fadeOut("slow");
+    $(".reachOutText").fadeOut("slow");
+    $(".aboutText").fadeIn("slow");
     $('#home').css({"color": "#777777"});
     $('#about').css({"color": "#009EDB"});
     $('#reachOut').css({"color": "#777777"});
 }, false);
 
 document.getElementById('reachOut').addEventListener('click', function(event) {
-    $(".introText").hide();
-    $(".aboutText").hide();
-    $(".reachOutText").show();
+    $(".introText").fadeOut("slow");
+    $(".aboutText").fadeOut("slow");
+    $(".reachOutText").fadeIn("slow");
     $('#home').css({"color": "#777777"});
     $('#about').css({"color": "#777777"});
     $('#reachOut').css({"color": "#009EDB"});
 }, false);
+
+$(".downArrow").click(function() {
+	$('html,body').animate({
+	    scrollTop: $(".projectsPage").offset().top},
+	    'slow');
+});
